5e01abf0f71b5a3eeda42eb298c597cc34b3c7bb
Alejandra Contreras <user.email>
2025-01-05 01:06:06 -0300

    Nuevos packege y clases

--- src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -4,9 +4,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.aluracursos.screenmatch.model.DatosSerie;
-import com.aluracursos.screenmatch.service.ConsumoAPI;
-import com.aluracursos.screenmatch.service.ConvierteDatos;
+import com.aluracursos.screenmatch.principal.Principal;
+
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -17,14 +16,9 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		var consumoApi = new ConsumoAPI();
-		var json = consumoApi.obtenerDatos(url: "https://www.omdbapi.com/?t=Game+of+Thrones&apikey=b9736b44");
-	//	var json = consumoApi.obtenerDatos(url: "https://coffee.alexflipnote.dev/random");
-		System.out.print(json);
-		ConvierteDatos conversor = new ConvierteDatos();
-		var datos = conversor.obtenerDatos(json, DatosSerie.class);
-		System.out.print(datos);
+		Principal principal = new Principal();
+		principal.muestraElMenu();
+		}
 	}
 
-}
+
--- src/main/java/com/aluracursos/screenmatch/service/ConvierteDatos.java
+++ src/main/java/com/aluracursos/screenmatch/service/ConvierteDatos.java
@@ -1,24 +1,20 @@
 package com.aluracursos.screenmatch.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ConvierteDatos implements IConvierteDatos{
 	
-	private ObjectMapper objectMapper = new ObjectMapper();
+	private ObjectMapper mapper = new ObjectMapper();
 
 	@Override
 	public <T> T obtenerDatos(String json, Class<T> clase) {
 		// TODO Auto-generated method stub
 		try {
-			return objectMapper.readValue(json, clase);
-		} catch (JsonMappingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			return mapper.readValue(json, clase);
 		} catch (JsonProcessingException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			throw new RuntimeException(e);
 		}
 	}
 	
